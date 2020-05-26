8b5465dd2e06e710a85621ef597f009be7340667
Stefan Braunovic <stefan.braunovic55@gmail.com>
2020-05-26 21:36:36 +0200

    adding WeatherEngine component

--- src/App.js
+++ src/App.js
@@ -1,64 +1,16 @@
 import React, { useState, useEffect } from "react";
 
-import WeatherCard from "./components/weatherCard/component";
 import "./App.css";
+import WeatherEngine from "./components/WeatherEngine";
 
-function App() {
-  const [query, setQuery] = useState("Sydney,au");
-  const [weather, setWeather] = useState({
-    temp: null,
-    city: null,
-    condition: null,
-    country: null,
-  });
-
-  const data = async (q) => {
-    const apiRes = await fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=${q}&units=metric&APPID=f2c4e1c1bd156e0991e2a71ab08ca127`
-    );
-    const resJSON = await apiRes.json();
-    return resJSON;
-  };
-
-  const handleSearch = (e) => {
-    e.preventDefault();
-    data(query).then((res) => {
-      console.log("the temp is " + res.main.temp);
-      setWeather({
-        temp: res.main.temp,
-        city: res.name,
-        condition: res.weather[0].main,
-        country: res.sys.country,
-      });
-    });
-  };
-  useEffect(() => {
-    data(query).then((res) => {
-      console.log("the temp is " + res.main.temp);
-      setWeather({
-        temp: res.main.temp,
-        city: res.name,
-        condition: res.weather[0].main,
-        country: res.sys.country,
-      });
-    });
-  }, []);
-
+const App = () => {
   return (
     <div className="App">
-      <WeatherCard
-        temp={weather.temp}
-        condition={weather.condition}
-        city={weather.city}
-        country={weather.country}
-      />
-
-      <form>
-        <input value={query} onChange={(e) => setQuery(e.target.value)} />
-        <button onClick={(e) => handleSearch(e)}>Search</button>
-      </form>
+      <WeatherEngine location="Podgorica, me" />
+      <WeatherEngine location="Belgrade,rs" />
+      <WeatherEngine location="Amsterdam,nl" />
     </div>
   );
-}
+};
 
 export default App;
