bce866bd4f85c63018087c55bf27fa5836b91248
Stefan Braunovic <stefan.braunovic55@gmail.com>
2020-05-27 09:19:39 +0200

    Updates

---
+++ precache-manifest.d6feb8c78d08d8014378c1363497f619.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "840884ddc0f13b903370c6e4c186a381",
+    "url": "/Weather-React/index.html"
+  },
+  {
+    "revision": "fbc0509c07c6581b96ee",
+    "url": "/Weather-React/static/css/main.bcf5e08c.chunk.css"
+  },
+  {
+    "revision": "a9934e9408b431912267",
+    "url": "/Weather-React/static/js/2.328cfea8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Weather-React/static/js/2.328cfea8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbc0509c07c6581b96ee",
+    "url": "/Weather-React/static/js/main.417c43b2.chunk.js"
+  },
+  {
+    "revision": "46863336190c1a642b44",
+    "url": "/Weather-React/static/js/runtime-main.833aa9a1.js"
+  }
+]);
